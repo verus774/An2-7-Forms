da2248b8b4391a4015638f166598d17180181371
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-06-04 11:11:27 +0300

    complete task #2

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from './../../models/user';
+
 @Component({
   selector: 'app-signup-reactive-form',
   templateUrl: './signup-reactive-form.component.html',
   styleUrls: ['./signup-reactive-form.component.css']
 })
 export class SignupReactiveFormComponent implements OnInit {
+  countries: Array<string> = ['Ukraine', 'Armenia', 'Belarus', 'Hungary', 'Kazakhstan', 'Poland', 'Russia'];
+  user: User = new User();
+  userForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendProducts: new FormControl(true)
+    });
+  }
+
+  onSave() {
+    // Form model
+    console.log(this.userForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.getRawValue())}`);
   }
 
 }

135dafb7ca3dd2175c66247c1d95b16bcbb04f03
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-06-04 12:33:10 +0300

    complete task #9

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -23,21 +23,35 @@ export class SignupReactiveFormComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.buildForm();
+    // this.buildForm();
+    this.createForm();
   }
 
   private createForm() {
     this.userForm = new FormGroup({
-      firstName: new FormControl(),
+      firstName: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(3)],
+        updateOn: 'blur'
+      }),
       lastName: new FormControl(),
       email: new FormControl(),
+      phone: new FormControl(),
+      notification: new FormControl('email'),
+      serviceLevel: new FormControl('', {
+        validators: [CustomValidators.serviceLevel],
+        updateOn: 'blur'
+      }),
       sendProducts: new FormControl(true)
     });
   }
 
   private buildForm() {
     this.userForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      // firstName: ['', [Validators.required, Validators.minLength(3)]],
+      // It works!
+      firstName: new FormControl('', {validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur'}),
+      // It works since v7
+      // firstName: this.fb.control('', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur' }),
       lastName: [
         { value: 'Zhyrytskyy', disabled: false },
         [Validators.required, Validators.maxLength(50)]
--- src/app/validators/custom.validators.ts
+++ src/app/validators/custom.validators.ts
@@ -2,6 +2,8 @@ import { AbstractControl } from '@angular/forms';
 
 export class CustomValidators {
   static serviceLevel(c: AbstractControl): { [key: string]: boolean } | null {
+    console.log('Validator: serviceLevel is called');
+
     if (c.value !== undefined && (Number.isNaN(c.value) || c.value < 1 || c.value > 5)) {
       return {
         serviceLevel: true
